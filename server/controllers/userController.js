81ae8a2b82e3e7e6e849951ca1a459ca1dbddd64
Kevin Sun <kevin@kevins-macbook.home>
2020-03-14 22:16:23 -0400

    added middleware for submitting posts and following users
    Co-authored by: Meisze Yau <meiszey@gmail.com>

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -85,5 +85,57 @@ userController.getUser = (req, res, next) => {
     .catch ((err) => next(err));
 };
 
+/* Expects format of req.body and to  be:
+ *   {
+ *     "username":  <- this should probably be in req.cookies or something
+ *     "location": , eg paris
+ *     "category": , eg attraction
+ *     "rating": integer from 1 to 5,
+ *     "recommenation": , eg eiffel tower <- can think of a better name if confusing
+ *     "review_text": eg it was pointy
+ * 
+ *   }
+ 
+ */
+userController.submitReview = (req, res, next) => {
+  const { username, location, category, rating, recommendation, review_text } = req.body;
+  const str = `INSERT INTO "review" (created_by, location, category, rating, recommendation, review_text)
+               VALUES ($1, $2, $3, $4, $5, $6);`;
+  const params = [username, location, category, rating, recommendation, review_text];
+  db.query(str, params)
+  .then((data) => {
+    console.log('is this working????')
+    console.log(data);
+    return next();
+  })
+  .catch((err) => next(err));
+
+}
+
+/* Expects format of req.body for user1 to follow user2:
+ *   {
+ *     "username":  <- his should probably be in req.cookies or something,
+ *     "user2 id or username:  
+ *     
+ *  }
+ * 
+ * However you want to get to to us - could be req.params in case 1, req.body in case 2 below
+ *    1. user1 can visit user2's profile. on user2's profile, there's a "follow" button (if user1 visits profile/user2, serve SELECT * from review WHERE created_by = user2)
+ *    2. or is there just a search bar on user1's homepage to follow  user2?
+ * 
+ *  
+ 
+ */
+
+ userController.follow = (req, res, next) => {
+   const { user_id, followedUser } =  req.body;
+   const str = `INSERT INTO "follows" (user_id, followed_user) VALUES ($1, $2)`;
+   const params = [user_id, followedUser]
+   db.query(str, params)
+   .then((data) => {
+     return next();
+   })
+   .catch(err => next(err));
+ }
 
 module.exports = userController;
