51160776ac22b0603696a7c93385506d93752d9f
Eric Tang <jivesushi@yahoo.com>
2020-03-25 00:08:26 -0400

    first commit

--- client/index.js
+++ client/index.js
@@ -1,8 +1,8 @@
-import React, { forwardRef } from 'react'
-import App from './components/App'
+import React from 'react'
+import App from './App'
 import { render } from 'react-dom'
 import styles from './stylesheets/styles.scss'
-import { Link, Router, Route, Switch, withRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import history from './components/history';
 
 render(<Router history={history}><App /></Router>, document.getElementById('root'));
