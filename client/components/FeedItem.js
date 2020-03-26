377234d7e85de88dc524d9f47380d69503256e66
Zac H <zac.haluza@gmail.com>
2020-03-26 14:02:26 -0400

    replaced if-or statement with ternary statement

--- client/components/FeedItem.js
+++ client/components/FeedItem.js
@@ -4,28 +4,42 @@ import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 
 function FeedItem(props) {
   let heartIcon;
-  if (props.isLiked === false) heartIcon = <AiOutlineHeart onClick={(event) => props.handleLikeReview(event, props.id, props.isLiked)} style={{ cursor: 'pointer' }}/>
-  else heartIcon = <AiFillHeart onClick={(event) => props.handleLikeReview(event, props.id, props.isLiked)} style={{ cursor: 'pointer' }}/>
+  !props.isLiked
+    ? (heartIcon = (
+        <AiOutlineHeart
+          onClick={event =>
+            props.handleLikeReview(event, props.id, props.isLiked)
+          }
+          style={{ cursor: 'pointer' }}
+        />
+      ))
+    : (heartIcon = (
+        <AiFillHeart
+          onClick={event =>
+            props.handleLikeReview(event, props.id, props.isLiked)
+          }
+          style={{ cursor: 'pointer' }}
+        />
+      ));
   return (
-
-        <div className='feed-item'>
-            <div className='feed-item-top-container'>
-                 <span className='feed-item-prop'> <span className='bold'>{`${props.locationDetail}, `}</span>{props.location}</span>
-                 <span className='feed-item-prop'> Category: {props.category} </span>
-            </div>
-            <div className='feed-item-mid-container' >{props.recommendation}</div> 
-            <div className='feed-item-bottom-container'>
-                <span className='feed-item-prop'> Reviewed by: {props.username} </span>
-                <span className='feed-item-prop'> Rating: {props.rating} </span>
-            </div>
-            <div className = 'review-text-box'>
-                {props.review_text}
-            </div>
-            {heartIcon}  {props.likes} likes
-        </div>
-    )
-
+    <div className="feed-item">
+      <div className="feed-item-top-container">
+        <span className="feed-item-prop">
+          {' '}
+          <span className="bold">{`${props.locationDetail}, `}</span>
+          {props.location}
+        </span>
+        <span className="feed-item-prop"> Category: {props.category} </span>
+      </div>
+      <div className="feed-item-mid-container">{props.recommendation}</div>
+      <div className="feed-item-bottom-container">
+        <span className="feed-item-prop"> Reviewed by: {props.username} </span>
+        <span className="feed-item-prop"> Rating: {props.rating} </span>
+      </div>
+      <div className="review-text-box">{props.review_text}</div>
+      {heartIcon} {props.likes} likes
+    </div>
+  );
 }
-
 
 export default FeedItem;
